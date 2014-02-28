72a93584c88cacda9662a4fe08edb8dfccb6d811
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2014-02-27 21:46:50 -0500

    scale down resolution to 640 x 640

--- Anypic/PAPEditPhotoViewController.m
+++ Anypic/PAPEditPhotoViewController.m
@@ -104,7 +104,7 @@
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
 
-    //[self shouldUploadImage:self.image];
+    [self shouldUploadImage:self.image];
 }
 
 #pragma mark - UITextFieldDelegate
