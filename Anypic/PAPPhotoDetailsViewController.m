b86dc1c63aa9fd5c25b9849e00f5262973771ce6
toboklee <toboklee@gmail.com>
2014-02-28 15:05:50 -0500

    small_change

--- Anypic/PAPPhotoDetailsViewController.m
+++ Anypic/PAPPhotoDetailsViewController.m
@@ -298,28 +298,6 @@ static const CGFloat kPAPCellInsetWidth = 7.5f;
 }
 
 
-#pragma mark - UIActionSheetDelegate
-
-- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
-    if (actionSheet.tag == MainActionSheetTag) {
-        if ([actionSheet destructiveButtonIndex] == buttonIndex) {
-            // prompt to delete
-            UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:NSLocalizedString(@"Are you sure you want to delete this photo?", nil) delegate:self cancelButtonTitle:NSLocalizedString(@"Cancel", nil) destructiveButtonTitle:NSLocalizedString(@"Yes, delete photo", nil) otherButtonTitles:nil];
-            actionSheet.tag = ConfirmDeleteActionSheetTag;
-            [actionSheet showFromTabBar:self.tabBarController.tabBar];
-        } else if ([actionSheet cancelButtonIndex] == buttonIndex){
-            // do nothing
-        } else {
-            [self activityButtonAction:actionSheet];
-        }
-    } else if (actionSheet.tag == ConfirmDeleteActionSheetTag) {
-        if ([actionSheet destructiveButtonIndex] == buttonIndex) {
-            
-            [self shouldDeletePhoto];
-        }
-    }
-}
-
 
 #pragma mark - UIScrollViewDelegate
 
@@ -340,18 +318,6 @@ static const CGFloat kPAPCellInsetWidth = 7.5f;
 
 -(void)photoDetailsHeaderView:(PAPPhotoDetailsHeaderView *)headerView didTapUserButton:(UIButton *)button user:(PFUser *)user {
     [self shouldPresentAccountViewForUser:user];
-}
-
-- (void)actionButtonAction:(id)sender {
-    UIActionSheet *actionSheet = [[UIActionSheet alloc] init];
-    actionSheet.delegate = self;
-    actionSheet.tag = MainActionSheetTag;
-    actionSheet.destructiveButtonIndex = [actionSheet addButtonWithTitle:NSLocalizedString(@"Delete Photo", nil)];
-    if (NSClassFromString(@"UIActivityViewController")) {
-        [actionSheet addButtonWithTitle:NSLocalizedString(@"Share Photo", nil)];
-    }
-    actionSheet.cancelButtonIndex = [actionSheet addButtonWithTitle:NSLocalizedString(@"Cancel", nil)];
-    [actionSheet showFromTabBar:self.tabBarController.tabBar];
 }
 
 - (void)activityButtonAction:(id)sender {
@@ -465,28 +431,4 @@ static const CGFloat kPAPCellInsetWidth = 7.5f;
 - (void)refreshControlValueChanged:(UIRefreshControl *)refreshControl {
     [self loadObjects];
 }
-/*
-- (BOOL)currentUserOwnsPhoto {
-    return [[[self.photo objectForKey:kPAPPhotoUserKey] objectId] isEqualToString:[[PFUser currentUser] objectId]];
-}
-*/
-
-- (void)shouldDeletePhoto {
-    // Delete all activites related to this photo
-    PFQuery *query = [PFQuery queryWithClassName:kPAPActivityClassKey];
-    [query whereKey:kPAPActivityPhotoKey equalTo:self.photo];
-    [query findObjectsInBackgroundWithBlock:^(NSArray *activities, NSError *error) {
-        if (!error) {
-            for (PFObject *activity in activities) {
-                [activity deleteEventually];
-            }
-        }
-        
-        // Delete photo
-        [self.photo deleteEventually];
-    }];
-    [[NSNotificationCenter defaultCenter] postNotificationName:PAPPhotoDetailsViewControllerUserDeletedPhotoNotification object:[self.photo objectId]];
-    [self.navigationController popViewControllerAnimated:YES];
-}
-
 @end
