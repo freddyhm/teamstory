4ff59ded079905277e0988abfc97ac56ce7527de
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2014-04-02 08:23:37 -0400

    Go to photo timeline when posting a pic, when there are multiple views in home

--- Anypic/PAPEditPhotoViewController.m
+++ Anypic/PAPEditPhotoViewController.m
@@ -139,9 +139,20 @@
     // get tab bar and home controller from stack
     PAPTabBarController *tabBarController =[[self.navigationController viewControllers] objectAtIndex:1];
     NSArray *tabBarViewControllers = [tabBarController viewControllers];
+    
+    // get home and phototimeline, if there are children pop 'em to get back to timeline
     PAPHomeViewController *homeViewController = [tabBarViewControllers objectAtIndex:0];
+    PAPPhotoTimelineViewController *photoViewController = [homeViewController.childViewControllers objectAtIndex:0];
+    
+    if([homeViewController.childViewControllers count] > 1){
+        [photoViewController.navigationController popViewControllerAnimated:NO];
+    }
+    
     [tabBarController setSelectedViewController:homeViewController];
     
+    NSArray *m = homeViewController.childViewControllers;
+    
+    [m objectAtIndex:0];
     // push tab bar with home controller now selected
     [self.navigationController popToViewController:tabBarController animated:YES];
 }
