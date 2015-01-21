db880af0fecbca52f792f775723ad12be3228388
Justin <toboklee@gmail.com>
2015-01-21 12:20:51 -0500

    photo time line done

--- Anypic/PAPPhotoCell.m
+++ Anypic/PAPPhotoCell.m
@@ -124,6 +124,8 @@
             [self.youtubeWebView loadHTMLString:[self setiFrameURLforYouTube:[self.ih_object objectForKey:@"link"]] baseURL:[[NSURL alloc] initWithString:[self.ih_object objectForKey:@"link"]]];
             self.captionLabel.frame = CGRectMake(12.0f, youtubeFrame + 10.0f, 295.0f, expectedSize.height + 15.0f);
             self.backgroundView.frame = CGRectMake(0.0f, 0.0f, 320.0f, youtubeFrame + self.captionLabel.frame.size.height + 20.0f);
+        } else {
+            [self.youtubeWebView removeFromSuperview];
         }
         
         
@@ -155,15 +157,15 @@
             [self.youtubeWebView loadHTMLString:[self setiFrameURLforYouTube:[self.ih_object objectForKey:@"link"]] baseURL:[[NSURL alloc] initWithString:[self.ih_object objectForKey:@"link"]]];
             [self.footerView setFrame:CGRectMake(0.0f, 205.0f, self.bounds.size.width, 44.0f)];
             
-            [self.footerView setFrame:CGRectMake(0.0f, self.imageView.frame.origin.y + self.imageView.frame.size.height, self.bounds.size.width, 44.0f)];
+            [self.footerView setFrame:CGRectMake(0.0f, 205.0f, self.bounds.size.width, 44.0f)];;
         } else {
             [self.footerView setFrame:CGRectMake(0.0f, self.imageView.frame.origin.y + self.imageView.frame.size.height, self.bounds.size.width, 44.0f)];
+            self.backgroundView.frame = CGRectMake(0.0f, 0.0f, 320.0f, self.imageView.frame.size.height + 10.0f);
+            [self.youtubeWebView removeFromSuperview];
+            [self.contentView bringSubviewToFront:self.imageView];
         }
-        
-        self.backgroundView.frame = CGRectMake(0.0f, 0.0f, 320.0f, self.imageView.frame.size.height + 10.0f);
     }
     
-    [self.contentView bringSubviewToFront:self.imageView];
     [self.contentView bringSubviewToFront:self.footerView];
     
     if ([[self.ih_object objectForKey:@"type"] isEqualToString:@"link"]) {
