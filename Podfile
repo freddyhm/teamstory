57e5a1fa71d1633df17ded5d6dcb0ee48ef1f7e5
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2015-02-03 17:22:56 -0800

    Pod update, remove mixpanel pod

--- Podfile
+++ Podfile
@@ -1,6 +1,5 @@
 platform :ios, '7.1'
 pod 'embedly-ios'
-pod 'Mixpanel'
 pod 'FlightRecorder'
 
 xcodeproj 'Teamstory.xcodeproj'
--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -52,19 +52,7 @@
 #define COCOAPODS_POD_AVAILABLE_FlightRecorder
 #define COCOAPODS_VERSION_MAJOR_FlightRecorder 1
 #define COCOAPODS_VERSION_MINOR_FlightRecorder 6
-#define COCOAPODS_VERSION_PATCH_FlightRecorder 0
-
-// Mixpanel
-#define COCOAPODS_POD_AVAILABLE_Mixpanel
-#define COCOAPODS_VERSION_MAJOR_Mixpanel 2
-#define COCOAPODS_VERSION_MINOR_Mixpanel 7
-#define COCOAPODS_VERSION_PATCH_Mixpanel 1
-
-// Mixpanel/MPCategoryHelpers
-#define COCOAPODS_POD_AVAILABLE_Mixpanel_MPCategoryHelpers
-#define COCOAPODS_VERSION_MAJOR_Mixpanel_MPCategoryHelpers 2
-#define COCOAPODS_VERSION_MINOR_Mixpanel_MPCategoryHelpers 7
-#define COCOAPODS_VERSION_PATCH_Mixpanel_MPCategoryHelpers 1
+#define COCOAPODS_VERSION_PATCH_FlightRecorder 2
 
 // embedly-ios
 #define COCOAPODS_POD_AVAILABLE_embedly_ios
