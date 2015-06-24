c5aa998648bcd7bf4ee553a8779918f5864a0c26
Freddy Hidalgo-Monchez <me@freddyhm.com>
2015-06-23 21:08:52 -0700

    Change width for project depending on where header is being called (for now only in activitypoints)

--- Anypic/PAPPhotoHeaderView.h
+++ Anypic/PAPPhotoHeaderView.h
@@ -24,6 +24,7 @@ typedef enum {
  @param buttons A bitmask specifying the interaction elements which are enabled in the view
  */
 - (id)initWithFrame:(CGRect)frame buttons:(PAPPhotoHeaderButtons)otherButtons;
+- (id)initWithFrame:(CGRect)frame buttons:(PAPPhotoHeaderButtons)otherButtons type:(NSString *)headerType;
 - (void)setUserForHeaderView:(PFUser *)auser;
 - (void)setForActivityPointView:(PFUser *)aUser;
 - (void)setPhoto:(PFObject *)aPhoto;
